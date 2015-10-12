953bed2b66360a6788d402287ed082351a4e5c51
nxtstep101 <nxtstep101@gmail.com>
2015-10-12 12:08:57 -0500

    Update main.php

--- main.php
+++ main.php
@@ -1,36 +1,42 @@
 <?php
-//defining initial values
-$level = 1;
-$gear = 0;
-$strength = $level + $gear;
-//checks which button was clicked
-if (isset($_POST['lvl_in'])) {
-	$level++;
+function counterStart()
+{
+	session_start();
+	$_SESSION['level'] = ((isset($_SESSION['level'])) ? $_SESSION['level'] : 1);
+	$_SESSION['gear'] = ((isset($_SESSION['gear'])) ? $_SESSION['gear'] : 0);
+	$_SESSION['strength'] = $_SESSION['level'] + $_SESSION['gear'];
+	if (isset($_POST['lvl_in'])) {
+		$_SESSION['level']++;
+	}
+	elseif (isset($_POST['lvl_de']) && $level > 1) {
+		$_SESSION['level'] = $_SESSION['level'] - 1;
+	}
+	elseif (isset($_POST['gear_in'])) {
+		$_SESSION['gear']++;
+	}
+	elseif (isset($_POST['gear_de']) && $gear > 0) {
+		$_SESSION['gear'] = $_SESSION['gear'] - 1;
+	}
+	else {
+		session_unset();
+	}
 }
-elseif (isset($_POST['lvl_de']) && $level > 1) {
-	$level = $level - 1;
-}
-elseif (isset($_POST['gear_in'])) {
-	$gear++;
-}
-elseif (isset($_POST['gear_de']) && $gear > 0) {
-	$gear = $gear - 1;
-}
+counterStart();
 ?>
 <!DOCTYPE html>
 <html>
    <body>
       <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
          <b>Level</b><br />
-         <?php print $level;?>
-         <input type="submit" name="lvl_in" value="+"><br />
+         <?php print $_SESSION['level'];?><br />
+         <input type="submit" name="lvl_in" value="+">
          <input type="submit" name="lvl_de" value="-">
          <b>Gear</b><br />
-         <?php print $gear;?>
-         <input type="submit" name="gear_in" value="+"><br />
+         <?php print $_SESSION['gear'];?><br />
+         <input type="submit" name="gear_in" value="+">
          <input type="submit" name="gear_de" value="-">
          <b>Strength</b><br />
-         <?php print $strength;?>
+         <?php print $_SESSION['strength'];?>
       </form>
    </body>
 </html>
