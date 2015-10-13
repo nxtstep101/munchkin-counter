36267074e2c3e81e1ebba847d4bd6b3ea144da11
nxtstep101 <nxtstep101@gmail.com>
2015-10-12 20:03:59 -0500

    Update main.php

--- main.php
+++ main.php
@@ -4,8 +4,8 @@ function counterStart()
 	//starts session
 	session_start();
 	//defines initial values
-	$_SESSION['level'] = ((isset($_SESSION['level'])) ? $_SESSION['level'] : 1);
-	$_SESSION['gear'] = ((isset($_SESSION['gear'])) ? $_SESSION['gear'] : 0);
+	$_SESSION['level']    = ((isset($_SESSION['level'])) ? $_SESSION['level'] : 1);
+	$_SESSION['gear']     = ((isset($_SESSION['gear'])) ? $_SESSION['gear'] : 0);
 	$_SESSION['strength'] = $_SESSION['level'] + $_SESSION['gear'];
 	//checks which button was clicked
 	if (isset($_POST['lvl_in'])) {
@@ -20,7 +20,7 @@ function counterStart()
 	elseif (isset($_POST['gear_de']) && $_SESSION['gear'] > 0) {
 		$_SESSION['gear']--;
 	}
-	else {
+	elseif (isset($_POST['restart'])) {
 		session_unset();
 	}
 }
@@ -30,16 +30,17 @@ counterStart();
 <html>
    <body>
       <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-         <b>Level</b><br />
-         <?php print $_SESSION['level'];?><br />
+         <b>Level:</b>
+	 <?php print $_SESSION['level'];?><br />
          <input type="submit" name="lvl_in" value="+">
-         <input type="submit" name="lvl_de" value="-">
-         <b>Gear</b><br />
+         <input type="submit" name="lvl_de" value="-"><br />
+         <b>Gear:</b>
          <?php print $_SESSION['gear'];?><br />
          <input type="submit" name="gear_in" value="+">
-         <input type="submit" name="gear_de" value="-">
-         <b>Strength</b><br />
-         <?php print $_SESSION['strength'];?>
+         <input type="submit" name="gear_de" value="-"><br />
+         <b>Strength:</b>
+         <?php print $_SESSION['strength'];?><br />
+         <input type="submit" name="restart" value="Restart">
       </form>
    </body>
 </html>
