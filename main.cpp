01bfdaf7d8e9b33fec081db4977ad3df5db01712
nxtstep101 <nxtstep101@gmail.com>
2017-12-11 00:00:20 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string userName;
+	
+	cout << "Welcome to Munchkin Level Counter" << endl;
+	cout << "Please enter your name: "
+	cin >> userName;
+	
+	cout << "Hello " userName << "!" << endl;
+	do {
+		cout << userName << "'s" << " stats:" << endl;
+		cout << "
+	} while (userChoice =! 4);
+}
